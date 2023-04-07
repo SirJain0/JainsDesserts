c4352f564b5de588a8a031ca74613566137e47e6
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-04-07 10:27:19 -0400

    Set up dev environment

---
+++ src/main/java/net/sirjain/tiramisu/TiramisuMod.java
@@ -0,0 +1,12 @@
+package net.sirjain.tiramisu;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TiramisuMod implements ModInitializer {
+	public static final String MOD_ID = "tiramisu";
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello, Tiramisu Mod!");
+	}
+}
