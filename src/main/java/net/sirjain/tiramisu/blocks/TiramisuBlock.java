556aad1b73ffb66a1717adc4184b70e6ee95e512
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-04-07 15:30:57 -0400

    Add voxel shape to block

--- src/main/java/net/sirjain/tiramisu/blocks/TiramisuBlock.java
+++ src/main/java/net/sirjain/tiramisu/blocks/TiramisuBlock.java
@@ -1,9 +1,21 @@
 package net.sirjain.tiramisu.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 
 public class TiramisuBlock extends Block {
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(2, 0, 2, 14, 7, 14);
+
     public TiramisuBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 }
