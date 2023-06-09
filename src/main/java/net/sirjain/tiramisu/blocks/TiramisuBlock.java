33171a4b6d90ebcba0bff5191c671e1290c27f52
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-06-09 18:08:00 -0400

    Adjust variant models

--- src/main/java/net/sirjain/tiramisu/blocks/TiramisuBlock.java
+++ src/main/java/net/sirjain/tiramisu/blocks/TiramisuBlock.java
@@ -22,10 +22,10 @@ import net.minecraft.world.event.GameEvent;
 public class TiramisuBlock extends CakeBlock {
     public static final VoxelShape SHAPE_1 = Block.createCuboidShape(2, 0, 2, 14, 7, 14);
     public static final VoxelShape SHAPE_2 = Block.createCuboidShape(2, 0, 2, 14, 7, 14);
-    public static final VoxelShape SHAPE_3 = Block.createCuboidShape(2, 0, 6, 14, 7, 14);
-    public static final VoxelShape SHAPE_4 = Block.createCuboidShape(2, 0, 6, 14, 7, 14);
-    public static final VoxelShape SHAPE_5 = Block.createCuboidShape(2, 0, 10, 14, 7, 14);
-    public static final VoxelShape SHAPE_6 = Block.createCuboidShape(2, 0, 10, 14, 7, 14);
+    public static final VoxelShape SHAPE_3 = Block.createCuboidShape(2, 0, 2, 14, 7, 14);
+    public static final VoxelShape SHAPE_4 = Block.createCuboidShape(8, 0, 2, 14, 7, 14);
+    public static final VoxelShape SHAPE_5 = Block.createCuboidShape(8, 0, 6, 14, 7, 14);
+    public static final VoxelShape SHAPE_6 = Block.createCuboidShape(8, 0, 10, 14, 7, 14);
     public static final VoxelShape SHAPE_7 = Block.createCuboidShape(8, 0, 10, 14, 7, 14);
     public static VoxelShape[] BITES_TO_SHAPE = new VoxelShape[] {SHAPE_1, SHAPE_2, SHAPE_3, SHAPE_4, SHAPE_5, SHAPE_6, SHAPE_7};
 
@@ -50,7 +50,7 @@ public class TiramisuBlock extends CakeBlock {
             int bitesState = state.get(BITES);
             world.emitGameEvent(player, GameEvent.EAT, pos);
 
-            if (bitesState < 6) {
+            if (bitesState < 5) {
                 world.setBlockState(pos, state.with(BITES, bitesState + 1), 3);
             } else {
                 world.removeBlock(pos, false);
