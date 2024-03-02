d44748e98909ebe62278a74be0abf31dd3d948b0
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-03-01 21:09:13 -0500

    Add cutouts, missing langs, and fix missing textures

---
+++ src/main/java/net/sirjain/jains_desserts/client/JainsDessertsClient.java
@@ -0,0 +1,14 @@
+package net.sirjain.jains_desserts.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.sirjain.jains_desserts.blocks.JDBlocks;
+
+public class JainsDessertsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		BlockRenderLayerMap.INSTANCE.putBlock(JDBlocks.SWEET_BERRY_PIE, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(JDBlocks.BANANA_CREAM_PIE, RenderLayer.getCutout());
+	}
+}
